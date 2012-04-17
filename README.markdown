d59442d21e9a01776ed2171b0568eaea449aded8
Mats Lindh <mats@lindh.no>
2012-04-18 01:11:08 +0200

    Add base version of deck.ignite.js

---
+++ README.markdown
@@ -0,0 +1,33 @@
+# deck.js-ignite
+Extension for deck.js to run your slide deck as an Ignite or Pecha Kucha
+presentation (or any other pre-timed variation).
+
+## License
+Licensed under the MIT license.
+
+## Usage
+Place deck.ignite.js in an extensions/ignite directory under your deck.js
+installation (you'll have to create this directory) (or a different 
+location if you prefer - this is your world, after all).
+
+Add a reference to the deck.ignite.js file after you've loaded deck.core.js:
+
+    <script type="text/javascript" src="deck.js/extensions/ignite/deck.ignite.js"></script>
+
+Then simply start your presentation - the slide deck will progress 
+automagically after a configurable delay (15 seconds by default for 
+Ignite, set to 20 seconds for Pecha Kucha).
+
+You can configure the delay when initializing deck.js:
+
+     $.deck(".slide", { igniteDelay: 20 });
+
+.. will use a slide delay of 20 seconds instead of 15.
+
+deck.ignite.js tries to get out of your way if you suddenly start to 
+navigate your slide deck manually. If you move backwards, it will exit 
+ignite mode and not active it again until you've waited at least the
+configured delay on a slide before moving forward in your deck (so to
+restart after navigating to a different slide, wait 15 seconds and then
+move manually forward. Ignite mode should automagically resume.)
+
---
+++ deck.ignite.js
@@ -0,0 +1,71 @@
+/*!
+An extension for deck.js to run as presentation as an ignite 
+presentation. Each slide will be shown for 15 seconds before the 
+slide deck changes to the next slide automagically.
+
+The first slide will be shown until the presentation is started,
+and the last slide will end the show. Any manual navigation during
+the slide show will cancel ignite mode, until a slide has been shown
+for at least 15 seconds. The next forward move will yet again start 
+the slide deck in ignite mode.
+
+Copyright (c) 2012 Mats Lindh
+Licensed under the MIT license.
+*/
+
+/*
+Runs a slide deck in ignite mode
+*/
+(function($, deck, undefined) {
+	var lastChange = 0;
+	var igniteActive = false;
+	var intervalTimer = null;
+
+	$.extend(true, $[deck].defaults, {
+		igniteDelay: 15
+	});
+		
+	$(document).bind('deck.change', function(event, from, to) {
+		function stopIgniteMode()
+		{
+			igniteActive = false;
+			clearInterval(intervalTimer);
+		}
+
+		var slideCount = $[deck]('getSlides').length;
+		var opts = $[deck]('getOptions');
+
+		// last slide ?
+		if (to == (slideCount - 1))
+		{
+			stopIgniteMode();
+		}
+		// moving forward in the slide deck
+		else if ((from+1) == to)
+		{
+			var currentTime = (new Date).getTime();
+
+			// start ignite mode if not already active and
+			// if we've been idle for more than 15 seconds
+			if (!igniteActive && ((currentTime - lastChange) >= (opts.igniteDelay * 1000)))
+			{
+				igniteActive = true;
+				intervalTimer = setInterval(function() {
+					$[deck]('next');
+				}, opts.igniteDelay * 1000);
+			}
+		}
+		else if ((to == 0) || (to < from))
+		{
+			stopIgniteMode();
+
+			if (to == 0)
+			{
+				// make sure we start again if we move forward
+				// from the first slide
+				currentTime = 0;
+			}
+		}
+	});
+})(jQuery, 'deck');
+
